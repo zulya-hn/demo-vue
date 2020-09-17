1f95f3132d3e1fa7b9f17f32a961410685ce0a3f
zulya <zulya.h95@gmail.com>
2020-09-17 23:42:43 +0300

    data print example;

--- app/resources/js/vue/components/app-root.vue
+++ app/resources/js/vue/components/app-root.vue
@@ -1,14 +1,60 @@
 <template>
-    <h1>
-        {{ title }}
-    </h1>
+    <div>
+        <h1>
+            {{ title }}
+        </h1>
+        <p>
+            {{ text }}
+        </p>
+        <ul>
+            <li v-for="user of usersList">
+                <h3>
+                    {{ user.name }}
+                </h3>
+                <p>
+                    Возраст: {{ user.age }}
+                </p>
+                <p>
+                    {{ user.phoneNumber }}
+                </p>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                title: 'We did it!'
+                title: 'We did it!',
+                text: 'git is works now!',
+                usersList: [
+                    {
+                        name: 'Павел',
+                        age: 25,
+                        phoneNumber: '45645745754'
+                    },
+                    {
+                        name: 'Денис',
+                        age: 67,
+                        phoneNumber: '4574575468767978'
+                    },
+                    {
+                        name: 'Юля',
+                        age: 34,
+                        phoneNumber: '43534636'
+                    },
+                    {
+                        name: 'Катя',
+                        age: 28,
+                        phoneNumber: '345637'
+                    },
+                    {
+                        name: 'Роман',
+                        age: 55,
+                        phoneNumber: '657465спроагно'
+                    },
+                ]
             }
         }
     }
